5b88cff6632816bc36bd97e1d171fdc72aa5a116
Monika <os_mo@wp.pl>
2023-11-12 15:04:51 +0100

    supabase authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { createClient } from "@supabase/supabase-js";
-const supabase = createClient("url", "api_key");
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import Home from './Home'
+import { supabase } from './supabase'
+import { Session } from "@supabase/gotrue-js/src/lib/types"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [session, setSession] = useState<Session | null>(null);
 
-  async function getLovePoint() {
-    const { data } = await supabase.from("love_points").select();
-    console.log(data)
-  }
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
 
-  async function getHatePoint() {
-    const { data } = await supabase.from("hate_points").select();
-    console.log(data)
-  }
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
 
-  
-  async function setLovePoint() {
-    const { data } = await supabase.from("love_points").insert({name: "Added new value"});
-    console.log(data)
-  }
+    return () => subscription.unsubscribe()
+  }, [])
 
-  async function editLovePoint() {
-    const { data } = await supabase.from("love_points").update({ name: 'Edited value' }).eq('id', '64c137fb-2d91-45c2-b5a7-ae3d845f9aef');
-    console.log(data)
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
   }
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => getLovePoint()}>
-          Get Love Points
-        </button>
-        <button onClick={() => setLovePoint()}>
-          Set Love Point
-        </button>
-        <button onClick={() => editLovePoint()}>
-          Edit Love Point
-        </button>
-        <button onClick={() => getHatePoint()}>
-          Get Hate Points
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  else {
+    return <Home />
+  }
 }
 
-export default App
+
+export default App;
